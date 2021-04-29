31a1ee024c810206ff44d787b556585a45b5e7ab
Hector <hmreluz@gmail.com>
2021-04-29 11:32:34 +1000

    1.Create a script to add 10 dummy vehicles to the database

---
+++ tests/Feature/Api/Check10VehiclesTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use App\Models\Vehicle;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class Check10VehiclesTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function test_generate_10_dummy_vehicles() {
+        $this->createVehicles();
+        $this->assertEquals(Vehicle::get()->count(), 10);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,21 @@
 
 namespace Tests;
 
+use App\Models\Vehicle;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function createVehicles() {
+
+        for ($i = 1 ; $i <= 10 ; $i++) {
+            $vehicle = new Vehicle();
+            $vehicle->brand    = "Brand ${i}";
+            $vehicle->name     = "Name ${i}";
+
+            $vehicle->save();
+        }
+    }
 }
